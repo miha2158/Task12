09f33ac1f1817aabf4b2dab24857818b491ad3f6
Mikhail <miha.vanya@gmail.com>
2017-04-17 00:54:29 +0500

    start

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    class Program
+    {
+        static int[] InsertionSort(int[] array, out int comparisons, out int swaps)
+        {
+            comparisons = 0;
+            swaps = 0;
+
+
+
+            return array;
+        }
+
+        static int[] CocktailSort(int[] array, out int comparisons, out int swaps)
+        {
+            comparisons = 0;
+            swaps = 0;
+
+
+
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
