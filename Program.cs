1366a4d0680bbc15794e0bed8a4476ba6e5f217f
miha2158 <miha.vanya@gmail.com>
2017-04-17 01:26:30 +0500

    algorithms done

--- Program.cs
+++ Program.cs
@@ -12,20 +12,70 @@ namespace Task12
         {
             comparisons = 0;
             swaps = 0;
+            int[] result = new int[array.Length];
+            array.CopyTo(result, 0);
 
+            for (int i = 1; i < result.Length; i++)
+            {
+                int temp = result[i];
+                int j = i;
 
+                while (j > 0 && result[j - 1] > temp)
+                {
+                    comparisons++;
+                    result[j] = result[--j];
+                    swaps++;
+                }
+                comparisons++;
+                result[j] = temp;
+            }
 
-            return array;
+            return result;
         }
 
         static int[] CocktailSort(int[] array, out int comparisons, out int swaps)
         {
             comparisons = 0;
             swaps = 0;
+            int[] result = new int[array.Length];
+            array.CopyTo(result, 0);
+            bool swapped;
 
+            do
+            {
+                swapped = false;
+                for (int i = 0; i <= result.Length - 2; i++)
+                {
+                    if(result[i] > result[i + 1])
+                    {
+                        comparisons++;
+                        int temp = result[i];
+                        result[i] = result[i + 1];
+                        result[i + 1] = temp;
+                        swaps++;
+                        swapped = true;
+                    }
+                }
 
+                if (!swapped)
+                    break;
+                swapped = false;
 
-            return array;
+                for (int i = result.Length - 2; i >= 0; i--)
+                {
+                    if (result[i] > result[i + 1])
+                    {
+                        comparisons++;
+                        int temp = result[i];
+                        result[i] = result[i + 1];
+                        result[i + 1] = temp;
+                        swaps++;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+
+            return result;
         }
 
         static void Main(string[] args)
